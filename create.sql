d1068fac46700604aaf9a21bb56b332982db50b1
Oskar Krygier <oskar.krygier@enterit.pl>
2023-04-29 20:58:54 +0200

    Add classes and instructors

--- create.sql
+++ create.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS gym_equipment, equipment, equipment_type, gym, pass, client, pass_client, employee, gym_employee;
+DROP TABLE IF EXISTS instructor, class, class_client, class_schedule;
 
 CREATE TABLE gym (
     id SERIAL,
@@ -66,3 +67,43 @@ CREATE TABLE gym_employee(
     FOREIGN KEY (id_gym) REFERENCES gym(id),
     FOREIGN KEY (id_employee) REFERENCES employee(id)
 );
+
+CREATE TABLE instructor (
+    id SERIAL,
+    name varchar(255) NOT NULL,
+    bio text,
+    photo varchar(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE class (
+    id SERIAL,
+    gym int NOT NULL,
+    name varchar(255) NOT NULL,
+    instructor int NOT NULL,
+    description text,
+    capacity int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (instructor) REFERENCES instructor(id),
+    FOREIGN KEY (gym) REFERENCES gym(id)
+);
+
+CREATE TABLE class_client (
+    id SERIAL,
+    class_id int NOT NULL,
+    client_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (class_id) REFERENCES class(id),
+    FOREIGN KEY (client_id) REFERENCES client(id)
+);
+
+CREATE TABLE class_schedule (
+    id SERIAL,
+    class_id int NOT NULL,
+    start_time time NOT NULL,
+    end_time time NOT NULL,
+    day_of_week int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (class_id) REFERENCES class(id)
+);
+
