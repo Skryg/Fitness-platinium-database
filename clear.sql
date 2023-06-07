bf280612d20a4287cb324f9b216142ca19f5c2d2
JohnBohnam <fanofledzeppelin@gmail.com>
2023-06-07 21:40:28 +0200

    wersja ostateczna bazy (hotfix)

--- clear.sql
+++ clear.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS 
 gym_equipment, equipment_type,
-gym, city, pass, client, pass_client, pass_gym,
+gym, city, pass, client, pass_client, pass_gym, gym_entry,
 employee, gym_employee, employee_user, person,
-blacklist, entry,
+blacklist, class_entry,
 challenge, award, challenge_award, 
 instructor, class, class_client, class_schedule, class_type,
 employee_schedule, default_employee_schedule, default_class_schedule, schedule
