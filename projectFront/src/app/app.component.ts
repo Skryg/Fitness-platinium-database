8626a8aaaea7bed8414b05a9ec3cf2b211a40925
Oskar Krygier <oskar.krygier@enterit.pl>
2023-06-10 22:47:18 +0200

    Merge to 'main'

--- projectFront/src/app/app.component.ts
+++ projectFront/src/app/app.component.ts
@@ -4,8 +4,6 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { finalize } from "rxjs/operators";
 import { ChangeDetectionStrategy } from '@angular/core';
-import { LoginService } from './service/login.service';
-import { PermissionService } from './service/permission.service';
 
 
 @Component({
@@ -16,47 +14,26 @@ import { PermissionService } from './service/permission.service';
 })
 export class AppComponent {
   title = 'Baza';
-<<<<<<< HEAD
   perm = 0;
-  constructor(private modalService: NgbModal,
-      private http: HttpClient, private router: Router,
-      public loginService: LoginService, 
-      private permissionService: PermissionService) {}
-=======
   selectedOption: string = '1';
   public static gymId: number = 0;
 
-  constructor(private modalService: NgbModal, private http: HttpClient, private router: Router) {
-
-  }
->>>>>>> 7a43a7b19a9486dda02210eeb786a49c9a0f1279
+  constructor(private modalService: NgbModal,
+      private http: HttpClient, private router: Router) {}
 
   public open(modal: any): void {
     this.modalService.open(modal);
   }
+  public updateGymId() : void {
+    AppComponent.gymId = parseInt(this.selectedOption);
+  }
 
-<<<<<<< HEAD
   public hasToken(){
     return sessionStorage.getItem('token') != null;
   }
 
-  permission() : Promise<any> {
-    return new Promise(resolve=>{
-      this.permissionService.getPermission().subscribe((data: any) => {
-        this.perm = data;
-        resolve(true);
-     });
-    });
-  }
-  
-  proceed(){
-    this.permission().then(value=>{alert("proceed");});
-  }
 
   
-=======
-  public updateGymId() : void {
-    AppComponent.gymId = parseInt(this.selectedOption);
-  }
->>>>>>> 7a43a7b19a9486dda02210eeb786a49c9a0f1279
+
+  
 }
