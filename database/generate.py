a6275bf8ffdd56cddb1be9c37af6cbd0c47ba8d3
JohnBohnam <fanofledzeppelin@gmail.com>
2023-06-13 04:03:04 +0200

    generatee stupid

--- database/generate.py
+++ database/generate.py
@@ -1,6 +1,7 @@
 import random
 import psycopg2
 from faker import Faker
+import enum
 
 fake = Faker('pl_PL')
 
@@ -96,38 +97,108 @@ def get_ids(tab_name):
     return occurring_ids
 
 
-def generate_relation(rel_name, col1_name, col2_name, tab1_name, tab2_name, des_count):
+def generate_relation(rel_name, col1_name, col2_name, tab1_name, tab2_name, des_count,
+                      debug=False,
+                      do_your_job=True):
     ids1 = get_ids(tab1_name)
     ids2 = get_ids(tab2_name)
     cnt = 0
+    actual_cnt = 0
+    conn = psycopg2.connect("dbname=postgres user=postgres password=postgres")
+    cur = conn.cursor()
     while cnt < des_count:
         id1 = random.choice(ids1)
         id2 = random.choice(ids2)
+        cnt += 1
         try:
-            conn = psycopg2.connect("dbname=postgres user=postgres password=postgres")
-            cur = conn.cursor()
-            #print(f"trying: {str(id1)}, {str(id2)}")
             cur.execute(f"insert into {rel_name} ({col1_name}, {col2_name}) values ({str(id1)}, {str(id2)})")
             conn.commit()
-            cnt+=1
-            #print("success")
+            actual_cnt += 1
+            if debug:
+                print("success")
         except psycopg2.DatabaseError as error:
+            conn.rollback()
+            if do_your_job:
+                cnt -= 1
+            if debug:
+                print(error)
             continue
+    conn.close()
+    print(f"inserted {str(actual_cnt)} simple relations {rel_name}({tab1_name}.{col1_name}, {tab2_name}.{col2_name})")
 
-    print("inserted " + str(cnt) + " passes")
+
+def fake_any(type):
+    if type == "address":
+        return fake.address()
+    if type == "name":
+        return fake.name()
+    if type == "surname":
+        return fake.surname()
+    if type == "date":
+        return fake.date_between(start_date='-1y', end_date='+1y').strftime('%Y-%m-%d')
 
 
+def generate_stupid(tab_name, ref_names, ref_tables, field_names, field_types,
+                    des_count,
+                    debug=False,
+                    do_your_job=False):
+    ids = []
+    for ref_table in ref_tables:
+        print("getting ids for" + ref_table)
+        ids.append(get_ids(ref_table))
+    cnt = 0
+    act_cnt = 0
+    conn = psycopg2.connect("dbname=postgres user=postgres password=postgres")
+    cur = conn.cursor()
+    sql_insert = f"INSERT INTO {tab_name} ("
+    for ref_name in ref_names:
+        sql_insert += f"{ref_name}, "
+    sql_insert += ", ".join(field_names)
+    sql_insert += ")"
 
+    if debug:
+        print(f"sql: {sql_insert}")
+
+    while cnt < des_count:
+        cnt += 1
+        try:
+            sql2 = sql_insert + " VALUES("
+            for act_ids in ids:
+                sql2 += f"{random.choice(act_ids)}, "
+            sql2 += ", ".join(fake_any(field_type) for field_type in field_types)
+            sql2 += ");"
+            print(sql2)
+
+        except psycopg2.DatabaseError as error:
+            conn.rollback()
+            if do_your_job:
+                cnt -= 1
+            if debug:
+                print(error)
+            continue
+    conn.close()
 
 
 if __name__ == '__main__':
-    print("start")
-    person_ids = generate_person(30)
-    client_ids = generate_simple(random.sample(person_ids, 25), "client")
-    employee_ids = generate_simple(random.sample(person_ids, 5), "employee")
-    instructor_ids = generate_simple(random.sample(employee_ids, len(employee_ids) * 5 // 10), "instructor",
-                                     "id_employee")
-    generate_passes(random.sample(client_ids, int(len(client_ids) * 0.9)), get_ids("pass"))
+    # print("start")
+    # person_ids = generate_person(30)
+    # client_ids = generate_simple(random.sample(person_ids, 25), "client")
+    # employee_ids = generate_simple(random.sample(person_ids, 5), "employee")
+    # instructor_ids = generate_simple(random.sample(employee_ids, len(employee_ids) * 5 // 10), "instructor",
+    #                                  "id_employee")
+    # generate_passes(random.sample(client_ids, int(len(client_ids) * 0.9)), get_ids("pass"))
+    #
+    # generate_relation("class_client", "id_class", "id_client", "class", "client",
+    #                   50, debug=False)
+    #
+    # generate_relation("class_entry", "id_client", "id_class_schedule", "client", "class_schedule",
+    #                   100, debug=False, do_your_job=False)
+    #
+    # generate_relation("gym_employee", "id_gym", "id_employee", "gym", "employee",
+    #                   30, debug=True)
 
-    generate_relation("class_entry", "id_client", "id_class_schedule", "client", "class_schedule", 10)
+    generate_stupid("tab",
+                    ["id_1", "id_2"], ["employee", "client"],
+                    ["imie1, address1"], ["name", str("date")],
+                    5)
 
